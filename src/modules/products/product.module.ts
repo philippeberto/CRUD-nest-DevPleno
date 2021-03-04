214a9a980e926e2740e554bc0699a331bf2bc8b1
philippeberto <philippeberto@outlook.com>
2021-03-04 19:44:43 +0000

    creating database module and using MySQLProvider

--- src/modules/products/product.module.ts
+++ src/modules/products/product.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ProductController } from './product.controller'
 import { ProductService } from './product.service'
-import { MySQLProvider } from 'src/database/mysql.provider'
 
 @Module({
   controllers: [ProductController],
-  providers: [MySQLProvider, ProductService],
+  providers: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule { }
--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { MySQLProvider } from 'src/database/mysql.provider'
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly mysql: MySQLProvider) {}
+  constructor(@Inject('DATABASE') private readonly mysql: MySQLProvider) { }
   async findAll(): Promise<any> {
+    console.log(this.mysql.getValue())
     return [{ id: 2 }, { id: 1 }, { id: 4 }, { id: 3 }, { id: 5 }]
   }
 }
